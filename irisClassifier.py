077bf61ffaef522fd7b19cab9703f82e73514fa9
Zatezalo <azatezalo@deloitte.ca>
2022-10-17 16:24:36 -0400

    Plot created.

--- irisClassifier.py
+++ irisClassifier.py
@@ -1,6 +1,8 @@
 import os
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
+import perceptron as perceptron
 
 # Getting Iris Photos
 s = 'https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data'
@@ -8,3 +10,16 @@ print('From URL: ', s)
 df = pd.read_csv(s, header=None, encoding='utf-8')
 
 # Getting first 100 Iris
+y = df.iloc[0:100, 4].values
+y = np.where(y == 'Iris-setosa', 0, 1)
+
+# Extract sepal length and petal length
+X = df.iloc[0:100, [0,2]].values
+
+# Plot Data
+plt.scatter(X[:50, 0], X[:50, 1], color='red', marker='o', label='Setosa')
+plt.scatter(X[50:100, 0], X[50:100, 1], color='blue', marker='o', label='Versicolor')
+plt.xlabel("Sepal length [cm]")
+plt.ylabel("Petal length [cm]")
+plt.legend(loc='upper left')
+plt.show()
