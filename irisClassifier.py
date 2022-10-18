d3e42112810b4dab14f3facbf36bf68d3b267c11
Aleksa Zatezalo <aleksa@kali.azatezalo>
2022-10-17 20:00:20 -0400

    Classifier complete.

--- irisClassifier.py
+++ irisClassifier.py
@@ -40,9 +40,9 @@ def plot_decision_regions(X, y, classifier, resolution=0.02):
     cmap = ListedColormap(colors[:len(np.unique(y))])
 
     # plot the decision surface
-    x1_min, x1_max = X[:,0].min() -1, X[:,0].max +1
-    x2_min, x2_max = X[:,1].min() -1, X[:,1].max +1
-    xx1, xx2 = np.meshgrid(np.arrange(x1_min, x1_max, resolution), np.arrange(x2_min, x2_max, resolution))
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max()
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max()
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution), np.arange(x2_min, x2_max, resolution))
     
     lab = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
     lab = lab.reshape(xx1.shape)
@@ -56,8 +56,11 @@ def plot_decision_regions(X, y, classifier, resolution=0.02):
             alpha=0.8,
             c=colors[idx],
             marker=markers[idx],
-            label=f'Class{c1}',
+            label=f'Class{cl}',
             edgecolor='black')
 
-
-
+plot_decision_regions(X, y, classifier=ppn)
+plt.xlabel=('Sepal Length [cm]')
+plt.ylabel=('Petal length [cm]')
+plt.legend(loc='upper left')
+plt.show()
